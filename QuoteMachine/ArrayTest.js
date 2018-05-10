75d380c39bb966ed805bbdbe192c5156bbe60260
XpFlyAK <565874880@qq.com>
2018-05-10 18:40:51 +0800

    change scroll doc

--- QuoteMachine/ArrayTest.js
+++ QuoteMachine/ArrayTest.js
@@ -1,16 +1,7 @@
-function unite(arr1, arr2, arr3) {
-    for(var  i=1 ;i<arguments.length;i++){
-
-        arguments[i].reduce(function(accumulator,currentValue){
-            if(currentValue in arr1){
-
-            }else{
-                arr1.push(currentValue);
-            }
-        });
-
-    }
-    return arr1;
+function convert(string) {
+    var reg = new RegExp("&");
+    return reg.test(string);
 }
 
-console.log(unite([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
+console.log(convert("Dolce & Gabbana"));
